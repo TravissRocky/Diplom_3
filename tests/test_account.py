06c86821982d77bf3876784c9b7061e9a4bdda80
Maxim Xim <eviefray@yandex.ru>
2025-11-18 20:40:35 +0700

    add test account

---
+++ tests/test_account.py
@@ -0,0 +1,30 @@
+import allure
+import pytest
+
+from pages.account_page import AccountPage
+from pages.main_page import MainPage
+
+
+@allure.feature('Личный кабинет')
+@pytest.mark.personal_account
+class TestPersonalAccount:
+    def test_open_personal_account(self, driver, base_url, authorized_user):
+        main_page = MainPage(driver)
+        main_page.open_personal_account()
+        assert '/account' in driver.current_url
+
+    def test_open_order_history(self, driver, base_url, authorized_user):
+        main_page = MainPage(driver)
+        main_page.open_personal_account()
+
+        account_page = AccountPage(driver)
+        account_page.open_order_history()
+        assert 'order-history' in driver.current_url
+
+    def test_logout_from_account(self, driver, base_url, authorized_user):
+        main_page = MainPage(driver)
+        main_page.open_personal_account()
+
+        account_page = AccountPage(driver)
+        account_page.logout()
+        assert '/login' in driver.current_url
